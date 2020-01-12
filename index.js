f6220259f6069df862193451307882c461184f17
James Zhong <thebitspud@gmail.com>
2020-01-12 05:22:50 -0800

    Edited twitch bot

--- index.js
+++ index.js
@@ -1,65 +1,48 @@
 const tmi = require('twitch-js');
 
 const options = {
-    options: {
-        debug: true,
-    },
-    connection: {
-        cluster: 'aws',
-        reconnect: true,
-    },
-    identity: {
-        username: 'inhotec',
-        password: 'oauth:j1n4zdgcho6hpfdt1ig279l9271jb1',
-    },
-    channels: ['epicyasuokid2007']
+	options: {
+		debug: true,
+	},
+	connection: {
+		cluster: 'aws',
+		reconnect: true,
+	},
+	identity: {
+		username: 'inhotec',
+		password: 'oauth:j1n4zdgcho6hpfdt1ig279l9271jb1',
+	},
+	channels: ['epicyasuokid2007']
 };
 
 const client = new tmi.client(options);
 
 client.connect();
 
-client.on('connected', (adress, port) => {
-    client.action('epicyasuokid2007', 'Hello, TGT Bot is now connected');
-});    
+client.on('connected', (address, port) => {
+	client.action('epicyasuokid2007', 'Greetings, summoner. Inhotec has been successfully deployed');
+});
 
 client.on('chat', (channel, user, message, self) => {
-    if (message === '!game') {
-        client.action('epicyasuokid2007', 'the game currently being played is INTERBOTS!');
-    }
-    
-    if (message === '!join') {
-        client.action('epicyasuokid2007', `${user} has joined the game!`);
-    }
+	if(message.substring(0, 1) === "!") message.slice(0, 1);
+	else return;
 
-    if (message === '!moveL') {
-        client.action('epicyasuokid2007', `${user} moved left!`);
-    }
-
-    if (message === '!moveR') {
-        client.action('epicyasuokid2007', `${user} moved right!`);
-    }
-    if (message === '!moveU') {
-        client.action('epicyasuokid2007', `${user} moved up!`);
-    }
-    if (message === '!moveD') {
-        client.action('epicyasuokid2007', `${user} moved down!`);
-    }
-
-    if (message === '!fire') {
-        client.action('epicyasuokid2007', `${user} fired!`);
-    }
-
-    if (message === '!respawn') {
-        client.action('epicyasuokid2007', `${user} has respawned!`);
-    }
-
-    if (message === '!leave') {
-        client.action('epicyasuokid2007', `${user} has disconnected from the game!`);
-    }
-
-    if (message === '!help') {
-        client.action('epicyasuokid2007', 'INTERBOTS COMMAND LIST: \n !join : Join the game , \n !moveL : Move left , \n moveR : Move right , \n moveU : Move Up , \n moveD : Move down , \n !fire : Fire/shoot , \n !respawn : Respawn after death in game , \n !leave : Leave the game');
-    }
-
+	switch(message) {
+		case "!game": client.action('epicyasuokid2007', 'The game currently being played is INTERBOTS!'); break;
+		case "!enter":
+		case "!join": client.action('epicyasuokid2007', `${user.username} has joined the game!`); break;
+		case "!move up": case "!mu":
+		case "!move U": client.action('epicyasuokid2007', `${user.username} moved up!`); break;
+		case "!move right": case "!mr":
+		case "!move R": client.action('epicyasuokid2007', `${user.username} moved right!`); break;
+		case "!move down": case "!md":
+		case "!move D": client.action('epicyasuokid2007', `${user.username} moved down!`); break;
+		case "!move left": case "!ml":
+		case "!move L": client.action('epicyasuokid2007', `${user.username} moved left!`); break;
+		case "!fire": client.action('epicyasuokid2007', `${user.username} fired!`);
+		case "!disconnect":
+		case "!leave": client.action('epicyasuokid2007', `${user.username} has disconnected from the game!`); break;
+		case "!help": client.action('epicyasuokid2007', '~~INTERBOTS COMMANDS~~ !join : Join the game | !ml : Move left | !mr : Move right | !mu : Move Up | !md : Move down | !fire : Shoot projectile | !leave : Disconnect'); break;
+		default: client.action('epicyasuokid2007', `I did not recognize ${user.username}'s command. Type !help for a list of available commands.`); break;
+	}
 });
