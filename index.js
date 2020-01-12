b222380d06a10e0195b5babe84b3cf027739cc40
Jason Lau <jasonlau5000@gmail.com>
2020-01-12 00:06:51 -0800

    Updated Twitch Responses
    Updated Twitch Responses

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const options = {
         reconnect: true,
     },
     identity: {
-        username: 'FitChallengeBot1',
-        password: 'oauth:b06jua4p6768y1ikzt49mrnit6edxk',
+        username: 'inhotec',
+        password: 'oauth:j1n4zdgcho6hpfdt1ig279l9271jb1',
     },
     channels: ['epicyasuokid2007']
 };
@@ -20,12 +20,46 @@ const client = new tmi.client(options);
 client.connect();
 
 client.on('connected', (adress, port) => {
-    client.action('epicyasuokid2007', 'Hello, FitChalleneBot1 is now connected');
+    client.action('epicyasuokid2007', 'Hello, TGT Bot is now connected');
 });    
 
 client.on('chat', (channel, user, message, self) => {
     if (message === '!game') {
-        client.action('epicyasuokid2007', 'epicyasuokid2007 is playing Minecraft!');
+        client.action('epicyasuokid2007', 'the game currently being played is INTERBOTS!');
     }
     
+    if (message === '!join') {
+        client.action('epicyasuokid2007', `${user} has joined the game!`);
+    }
+
+    if (message === '!moveL') {
+        client.action('epicyasuokid2007', `${user} moved left!`);
+    }
+
+    if (message === '!moveR') {
+        client.action('epicyasuokid2007', `${user} moved right!`);
+    }
+    if (message === '!moveU') {
+        client.action('epicyasuokid2007', `${user} moved up!`);
+    }
+    if (message === '!moveD') {
+        client.action('epicyasuokid2007', `${user} moved down!`);
+    }
+
+    if (message === '!fire') {
+        client.action('epicyasuokid2007', `${user} fired!`);
+    }
+
+    if (message === '!respawn') {
+        client.action('epicyasuokid2007', `${user} has respawned!`);
+    }
+
+    if (message === '!leave') {
+        client.action('epicyasuokid2007', `${user} has disconnected from the game!`);
+    }
+
+    if (message === '!help') {
+        client.action('epicyasuokid2007', 'INTERBOTS COMMAND LIST: \n !join : Join the game , \n !moveL : Move left , \n moveR : Move right , \n moveU : Move Up , \n moveD : Move down , \n !fire : Fire/shoot , \n !respawn : Respawn after death in game , \n !leave : Leave the game');
+    }
+
 });
